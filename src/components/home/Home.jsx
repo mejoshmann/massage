133205e0ce0dd1a82d53d9b06d368b0356706057
Josh Mann <goostcreative@gmail.com>
2024-05-22 11:16:54 -0700

    Add keywords to page for SEO

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,16 +14,18 @@ function Home() {
           </p>
           <p className="home__copy--copy">
             Indulge in the ultimate relaxation and therapeutic benefits of
-            professional massage therapy, all in the comfort of your own home, or at our Surrey location
+            professional massage therapy, all in the comfort of your own home,
+            or at our serene Surrey location.
           </p>
           <p className="home__copy--copy">
             <a href="#contact" className="home__copy--link">
               Contact us
             </a>{" "}
-            to experience the ease and convenience of mobile massage. Whether
-            you're looking to enhance your mobility, address an injury, or
-            simply relax and pamper yourself, Relax Massage Surrey is here to
-            help.{" "}
+            Our expert massage therapists specialize in a variety of
+            techniques to help you relax, recover from injury, and enhance your
+            mobility. Book your in-home massage therapy session today and
+            discover why we are the top choice for mobile massage services in
+            Surrey.
           </p>
         </div>
         <img
